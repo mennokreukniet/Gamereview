57b2bc76116bdf4861ae40cadd95339f608b7f66
Dylan de Leeuw <dylan.deleeuw@live.nl>
2016-06-15 09:37:01 +0200

    bezig met de css

--- huge-master/application/view/_templates/header.php
+++ huge-master/application/view/_templates/header.php
@@ -3,46 +3,53 @@
 <head>
     <title>HUGE</title>
     <!-- META -->
-    <meta charset="utf-8">
-    <!-- send empty favicon fallback to prevent user's browser hitting the server for lots of favicon requests resulting in 404s -->
-    <link rel="icon" href="data:;base64,=">
     <!-- CSS -->
     <link rel="stylesheet" href="<?php echo Config::get('URL'); ?>css/style.css" />
 </head>
 <body>
     <!-- wrapper, to center website -->
-    <div class="wrapper">
 
-        <!-- logo -->
-        <div class="logo"></div>
+    <div id="header" class="header">
+        <img src="../public/css/dinges.jpg" class="float">
 
         <!-- navigation -->
-        <ul class="navigation">
+        
+
+
+        <!-- my account -->
+        
+    </div>
+    <div class="red_bar"></div>
+
+</body>
+
+
+<!--<ul class="navigation">
             <li <?php if (View::checkForActiveController($filename, "index")) { echo ' class="active" '; } ?> >
                 <a href="<?php echo Config::get('URL'); ?>index/index">Index</a>
-            </li>
+            
             <li <?php if (View::checkForActiveController($filename, "profile")) { echo ' class="active" '; } ?> >
                 <a href="<?php echo Config::get('URL'); ?>profile/index">Profiles</a>
-            </li>
+            
             <?php if (Session::userIsLoggedIn()) { ?>
                 <li <?php if (View::checkForActiveController($filename, "dashboard")) { echo ' class="active" '; } ?> >
                     <a href="<?php echo Config::get('URL'); ?>dashboard/index">Dashboard</a>
-                </li>
+                
                 <li <?php if (View::checkForActiveController($filename, "note")) { echo ' class="active" '; } ?> >
                     <a href="<?php echo Config::get('URL'); ?>note/index">My Notes</a>
-                </li>
+                
             <?php } else { ?>
                 <!-- for not logged in users -->
                 <li <?php if (View::checkForActiveControllerAndAction($filename, "login/index")) { echo ' class="active" '; } ?> >
                     <a href="<?php echo Config::get('URL'); ?>login/index">Login</a>
-                </li>
+                
                 <li <?php if (View::checkForActiveControllerAndAction($filename, "register/index")) { echo ' class="active" '; } ?> >
                     <a href="<?php echo Config::get('URL'); ?>register/index">Register</a>
-                </li>
+                
             <?php } ?>
         </ul>
 
-        <!-- my account -->
+
         <ul class="navigation right">
         <?php if (Session::userIsLoggedIn()) : ?>
             <li <?php if (View::checkForActiveController($filename, "user")) { echo ' class="active" '; } ?> >
@@ -50,22 +57,22 @@
                 <ul class="navigation-submenu">
                     <li <?php if (View::checkForActiveController($filename, "user")) { echo ' class="active" '; } ?> >
                         <a href="<?php echo Config::get('URL'); ?>user/changeUserRole">Change account type</a>
-                    </li>
+                    
                     <li <?php if (View::checkForActiveController($filename, "user")) { echo ' class="active" '; } ?> >
                         <a href="<?php echo Config::get('URL'); ?>user/editAvatar">Edit your avatar</a>
-                    </li>
+                    
                     <li <?php if (View::checkForActiveController($filename, "user")) { echo ' class="active" '; } ?> >
                         <a href="<?php echo Config::get('URL'); ?>user/editusername">Edit my username</a>
-                    </li>
+                    
                     <li <?php if (View::checkForActiveController($filename, "user")) { echo ' class="active" '; } ?> >
                         <a href="<?php echo Config::get('URL'); ?>user/edituseremail">Edit my email</a>
-                    </li>
+                    
                     <li <?php if (View::checkForActiveController($filename, "user")) { echo ' class="active" '; } ?> >
                         <a href="<?php echo Config::get('URL'); ?>user/changePassword">Change Password</a>
-                    </li>
+                    
                     <li <?php if (View::checkForActiveController($filename, "login")) { echo ' class="active" '; } ?> >
                         <a href="<?php echo Config::get('URL'); ?>login/logout">Logout</a>
-                    </li>
+                    
                 </ul>
             </li>
             <?php if (Session::get("user_account_type") == 7) : ?>
@@ -73,7 +80,7 @@
                     echo ' class="active" ';
                 } ?> >
                     <a href="<?php echo Config::get('URL'); ?>admin/">Admin</a>
-                </li>
+               
             <?php endif; ?>
         <?php endif; ?>
-        </ul>
+        </ul>-->
--- huge-master/application/view/games/index.php
+++ huge-master/application/view/games/index.php
@@ -1,9 +1,6 @@
-<div class="container">
-		<h1>Games</h1>
-		<div class="box">
-
-        <!-- echo out the system feedback (error and success messages) -->
-        <?php $this->renderFeedbackMessages(); ?>
+<div class="content">
+<h1>Games</h1>
+    <!-- echo out the system feedback (error and success messages) -->
 
         <h3>All the games conveniently listed for you.</h3>
         <div>
@@ -14,19 +11,16 @@
                 <thead>
                 <tr>
                     <td>Game</td>
-
                 </tr>
                 </thead>
                 <?php foreach ($this->games as $game) { ?>
-                    <table>
-                		<td><?= $game->id; ?></td>
-                    	<td><?= $game->name; ?></td>
-                    </table>
-                	<?php 
-                		}
-                	?>
+                    <div class="game_block"
+                       <td> <img class="game_thumbnail" src="<?= $game->thumbnail; ?>">
+                        <?= $game->name; ?></td>
+                    </div>
+                    <?php 
+                        }
+                    ?>
             </table>
         </div>
-	</div>
 </div>
- 
