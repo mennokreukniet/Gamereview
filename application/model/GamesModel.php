a448e6dee87b7ed164130195b4d2e9c3c84caffc
mennokreukniet <menno.kreukniet@hotmail.com>
2016-06-23 09:47:36 +0200

    Couple of changes

--- application/model/GamesModel.php
+++ application/model/GamesModel.php
@@ -12,4 +12,15 @@ class GamesModel
 		
 		return $query->fetchAll();
     }
+
+    public static function filterGames($platform_id)
+    {
+    	$db = DatabaseFactory::getFactory()->getConnection();
+
+		$sql = "SELECT * FROM games WHERE platform_id";
+		$query = $db->prepare($sql);
+		$query->execute();
+		
+		return $query->fetchAll();
+    }
 }
--- application/view/games/index.php
+++ application/view/games/index.php
@@ -5,6 +5,36 @@
         <h3>All the games conveniently listed for you.</h3>
         <div>
             Every game we have information about is listed here.
+            <div class="dropdown">
+                <script>
+                    /* When the user clicks on the button,
+                    toggle between hiding and showing the dropdown content */
+                        function filter() {
+                           document.getElementById("dropdownfilter").classList.toggle("show");
+                        }
+
+                    // Close the dropdown if the user clicks outside of it
+                    window.onclick = function(event) {
+                      if (!event.target.matches('.dropbtn')) {
+
+                   var dropdowns = document.getElementsByClassName("dropdown-content");
+                    var i;
+                     for (i = 0; i < dropdowns.length; i++) {
+                         var openDropdown = dropdowns[i];
+                      if (openDropdown.classList.contains('show')) {
+                        openDropdown.classList.remove('show');
+                    }
+                }
+            }
+        }
+        </script>
+                <button onclick="filter()" class="icon filter dropbtn"> Filter</button>
+                <div id="dropdownfilter" class="dropdown-content">
+                    <a href="#">Xbox</a>
+                    <a href="#">Playstation</a>
+                    <a href="#">PC</a>
+                </div>
+            </div>
         </div>
 
         <div>
